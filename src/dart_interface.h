75eb54d47b334e36d956bb5de700890b99b4f3ad
sneurlax <sneurlax@gmail.com>
2023-11-10 17:27:02 -0600

    expand interface WIP

--- src/dart_interface.h
+++ src/dart_interface.h
@@ -9,15 +9,22 @@
     #define EXPORT_DART __declspec(dllexport)
 #endif
 
-EXPORT_DART
-const char* getAddress(const char* keyDataHex, int index, int diversifier, int isTestNet);
+ EXPORT_DART
+ const char *createFullViewKey(const char* keyData, int index);
 
-/*
-EXPORT_DART
-const char *createFullViewKey(const char* keyData, int index);
+ EXPORT_DART
+ const char* createIncomingViewKey(const char* keyData, int index);
 
-EXPORT_DART
-const char* createIncomingViewKey(const char* keyData, int index);
-*/
+ EXPORT_DART
+ const char* getAddress(const char* keyDataHex, int index, int diversifier, int isTestNet);
+
+ EXPORT_DART
+ const char* identifyCoin(const char* coinDataHex, const char* keyDataHex, int index, int diversifier);
+
+ EXPORT_DART
+ const char* createSparkMintRecipients(const char* keyDataHex, int index, int diversifier, int isTestNet, const char* outputsJson);
+
+ EXPORT_DART
+ const char* createSparkSpendTransaction(const char* keyDataHex, int index, int diversifier, int isTestNet, const char* recipientsJson, const char* privateRecipientsJson, const char* coinsJson, const char* coverSetDataJson, const char* idAndBlockHashesJson, const char* txHashSigHex);
 
 #endif //ORG_FIRO_SPARK_DART_INTERFACE_H
--- src/utils.h
+++ src/utils.h
@@ -3,20 +3,55 @@
 
 #include "../include/spark.h"
 
-const char* getAddressFromData(const char* keyData, int index, const uint64_t diversifier);
-
-spark::SpendKey createSpendKeyFromData(const char *keyData, int index);
-
 char const hexArray[16] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd',
 						   'e', 'f'};
 
-unsigned char *hex2bin(const char *str);
+unsigned char *hexToBytes(const char *str);
 
-const char *bin2hex(const unsigned char *bytes, int size);
+const char *bytesToHex(const unsigned char *bytes, int size);
 
-const char *bin2hex(const char *bytes, int size);
+const char *bytesToHex(const char *bytes, int size);
 
-const char *bin2hex(std::vector<unsigned char> bytes, int size);
+const char *bytesToHex(std::vector<unsigned char> bytes, int size);
 
+spark::SpendKey createSpendKeyFromData(const char* keyDataHex, int index);
+
+spark::Coin createCoinFromData(const char* coinDataHex, spark::Address address);
+
+uint256 createTxHashSigFromData(const char* txHashSigHex);
+
+std::vector<std::pair<CAmount, bool>> createRecipientsFromJson(const char* recipientsJson);
+
+std::vector<std::pair<spark::OutputCoinData, bool>> createPrivateRecipientsFromJson(const char* privateRecipientsJson);
+
+std::list<CSparkMintMeta> createCoinsFromJson(const char* coinsJson);
+
+std::vector<spark::MintedCoinData> createMintedCoinDataFromJson(const char* outputsJson, spark::Address address);
+
+std::unordered_map<uint64_t, spark::CoverSetData> createCoverSetDataFromJson(const char* coverSetDataJson);
+
+std::map<uint64_t, uint256> createIdAndBlockHashesFromJson(const char* idAndBlockHashesJson);
+
+std::string serializeFullViewKey(spark::FullViewKey fullViewKey);
+
+std::string serializeIncomingViewKey(spark::IncomingViewKey incomingViewKey);
+
+std::string serializeIdentifiedCoinData(spark::IdentifiedCoinData identifiedCoinData);
+
+std::string serializeSpendTransaction(std::vector<uint8_t> serializedSpend, std::vector<std::vector<unsigned char>> outputScripts);
+
+std::string serializeRecipients(std::vector<CRecipient> recipients);
+
+std::vector<std::pair<CAmount, bool>> parseRecipients(std::string json);
+
+std::vector<std::pair<spark::OutputCoinData, bool>> parsePrivateRecipients(std::string json);
+
+std::list<CSparkMintMeta> parseCoins(std::string json);
+
+std::vector<spark::MintedCoinData> parseMintedCoinData(std::string json, spark::Address address);
+
+std::unordered_map<uint64_t, spark::CoverSetData> parseCoverSetData(std::string json);
+
+std::map<uint64_t, uint256> parseIdAndBlockHashes(std::string json);
 
 #endif //ORG_FIRO_SPARK_UTILS_H
